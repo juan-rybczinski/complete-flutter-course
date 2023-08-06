76a69fe174d9be4b894b9d0a037dbd816efcbb5e
juan-rybczinski <jhreplay.lee@gmail.com>
2023-08-07 05:26:56 +0900

    Updating the LeaveReviewAction by reading read data from the userPurchaseProvider

--- ecommerce_app/lib/src/features/products/presentation/product_screen/leave_review_action.dart
+++ ecommerce_app/lib/src/features/products/presentation/product_screen/leave_review_action.dart
@@ -1,12 +1,13 @@
-import 'package:ecommerce_app/src/features/orders/domain/order.dart';
+import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
+import 'package:ecommerce_app/src/common_widgets/responsive_two_column_layout.dart';
+import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:ecommerce_app/src/features/orders/application/user_orders_provider.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/reviews/application/reviews_service.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
 import 'package:ecommerce_app/src/utils/date_formatter.dart';
 import 'package:flutter/material.dart';
-import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
-import 'package:ecommerce_app/src/common_widgets/responsive_two_column_layout.dart';
-import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -14,22 +15,13 @@ import 'package:go_router/go_router.dart';
 /// leave a review.
 class LeaveReviewAction extends ConsumerWidget {
   const LeaveReviewAction({super.key, required this.productId});
+
   final ProductID productId;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: Read from data source
-    final orders = [
-      Order(
-        id: 'abc',
-        userId: '123',
-        items: {productId: 1},
-        orderStatus: OrderStatus.confirmed,
-        orderDate: DateTime.now(),
-        total: 15.0,
-      )
-    ];
-    if (orders.isNotEmpty) {
+    final orders = ref.watch(matchingUserOrdersProvider(productId)).value;
+    if (orders != null && orders.isNotEmpty) {
       final dateFormatted =
           ref.watch(dateFormatterProvider).format(orders.first.orderDate);
       return Column(
@@ -45,16 +37,25 @@ class LeaveReviewAction extends ConsumerWidget {
             rowCrossAxisAlignment: CrossAxisAlignment.center,
             columnCrossAxisAlignment: CrossAxisAlignment.center,
             startContent: Text('Purchased on $dateFormatted'.hardcoded),
-            endContent: CustomTextButton(
-              text: 'Leave a review'.hardcoded,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Colors.green[700]),
-              onPressed: () => context.goNamed(
-                AppRoute.leaveReview.name,
-                pathParameters: {'id': productId},
-              ),
+            endContent: Consumer(
+              builder: (context, ref, _) {
+                final reviewValue =
+                    ref.watch(userReviewStreamProvider(productId));
+                return CustomTextButton(
+                  text: (reviewValue.value == null
+                          ? 'Leave a review'
+                          : 'Update review')
+                      .hardcoded,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(color: Colors.green[700]),
+                  onPressed: () => context.goNamed(
+                    AppRoute.leaveReview.name,
+                    pathParameters: {'id': productId},
+                  ),
+                );
+              },
             ),
           ),
           gapH8,
